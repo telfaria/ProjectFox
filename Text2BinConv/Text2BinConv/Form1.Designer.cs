90e889abb9953796aaca3fbbeaab2b28c1984fe1
telfaria <elfaria@tipsofvb.net>
2019-04-21 19:02:06 +0900

    Initial Commit

---
+++ Text2BinConv/Text2BinConv/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Text2BinConv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbEncode = new System.Windows.Forms.ToolStripButton();
+            this.txtIn = new System.Windows.Forms.TextBox();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            this.tsbDecode = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.toolStrip1);
+            this.splitContainer1.Panel1.Controls.Add(this.txtIn);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtOut);
+            this.splitContainer1.Size = new System.Drawing.Size(1200, 600);
+            this.splitContainer1.SplitterDistance = 245;
+            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbEncode,
+            this.tsbDecode});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 2, 0);
+            this.toolStrip1.Size = new System.Drawing.Size(1200, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsbEncode
+            // 
+            this.tsbEncode.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbEncode.Image = ((System.Drawing.Image)(resources.GetObject("tsbEncode.Image")));
+            this.tsbEncode.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbEncode.Name = "tsbEncode";
+            this.tsbEncode.Size = new System.Drawing.Size(50, 22);
+            this.tsbEncode.Text = "Encode";
+            this.tsbEncode.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.tsbEncode.Click += new System.EventHandler(this.tsbEncode_Click);
+            // 
+            // txtIn
+            // 
+            this.txtIn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtIn.Location = new System.Drawing.Point(4, 37);
+            this.txtIn.Margin = new System.Windows.Forms.Padding(4);
+            this.txtIn.Multiline = true;
+            this.txtIn.Name = "txtIn";
+            this.txtIn.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtIn.Size = new System.Drawing.Size(1194, 202);
+            this.txtIn.TabIndex = 0;
+            // 
+            // txtOut
+            // 
+            this.txtOut.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOut.Location = new System.Drawing.Point(0, -1);
+            this.txtOut.Margin = new System.Windows.Forms.Padding(4);
+            this.txtOut.Multiline = true;
+            this.txtOut.Name = "txtOut";
+            this.txtOut.ReadOnly = true;
+            this.txtOut.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOut.Size = new System.Drawing.Size(1198, 345);
+            this.txtOut.TabIndex = 0;
+            // 
+            // tsbDecode
+            // 
+            this.tsbDecode.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbDecode.Image = ((System.Drawing.Image)(resources.GetObject("tsbDecode.Image")));
+            this.tsbDecode.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbDecode.Name = "tsbDecode";
+            this.tsbDecode.Size = new System.Drawing.Size(51, 22);
+            this.tsbDecode.Text = "Decode";
+            this.tsbDecode.Click += new System.EventHandler(this.tsbDecode_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 600);
+            this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.TextBox txtIn;
+        private System.Windows.Forms.TextBox txtOut;
+        private System.Windows.Forms.ToolStripButton tsbEncode;
+        private System.Windows.Forms.ToolStripButton tsbDecode;
+    }
+}
+
---
+++ Text2BinConv/Text2BinConv/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Text2BinConv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tsbEncode_Click(object sender, EventArgs e)
+        {
+            TextEncode(txtIn.Text);
+        }
+
+        private void tsbDecode_Click(object sender, EventArgs e)
+        {
+            TextDecode(txtIn.Text);
+        }
+
+        private void TextDecode(string text)
+        {
+            //ビット列を読み込む
+            //4ビット分ずつ
+
+            txtOut.Clear();
+
+            string[] decodebytes;
+            decodebytes = StringExtensions.SubstringAtCount(text, 4);
+
+
+            byte[] inbytes = new byte[decodebytes.Length];
+
+            for (int i = 0; i < decodebytes.Length; i++)
+            {
+                txtOut.AppendText(GetStringFromBitString(decodebytes[i]));
+            }
+
+
+
+            //// バイト列になったので、これを戻す
+            //string dbgtxt = System.Text.Encoding.UTF8.GetString(inBytes);
+            //txtOut.AppendText(dbgtxt);
+
+
+
+
+
+        }
+
+        private void TextEncode(string inText)
+        {
+            txtOut.Clear();
+
+            /*
+             * 手順
+             * 1.テキストを読み込む
+             * 2.UTF-8のバイナリに変換
+             * 3.そのバイナリをバイト列に変換
+             * 4.バイト列をビットに変換(8文字ずつ区切る)
+             */
+
+
+
+            byte[] inBytes = System.Text.Encoding.UTF8.GetBytes(inText);
+
+            //output(debug)
+            for (int i = 0; i < inBytes.Length; i++)
+            {
+                txtOut.AppendText(inBytes[i].ToString());
+            }
+            /*
+            // バイト列から戻してみる
+            string dbgtxt = System.Text.Encoding.UTF8.GetString(inBytes);
+            txtOut.AppendText(dbgtxt);
+            */
+
+            //バイト列になったので、これをビット列に変換
+
+            string bytedata = txtOut.Text;
+
+            string[] sBytes = new string[bytedata.Length];
+
+            for (int i = 0; i < bytedata.Length; i++)
+            {
+                sBytes[i] = bytedata.Substring(i, 1);
+            }
+
+            //debug
+            for (int i = 0; i < sBytes.Length; i++)
+            {
+                txtOut.AppendText(sBytes[i]);
+            }
+
+
+
+            // 読み込んだらtxtOutを一旦クリア
+            txtOut.AppendText("\n");
+
+            for (int i = 0; i < inBytes.Length; i++)
+            {
+                txtOut.AppendText(GetBitStringFromString(sBytes[i]));
+            }
+        }
+
+        private string GetBitStringFromString(string v)
+        {
+            switch (v)
+            {
+                case "0":
+                    return "0000";
+                case "1":
+                    return "0001";
+                case "2":
+                    return "0010";
+                case "3":
+                    return "0011";
+                case "4":
+                    return "0100";
+                case "5":
+                    return "0101";
+                case "6":
+                    return "0110";
+                case "7":
+                    return "0111";
+                case "8":
+                    return "1000";
+                case "9":
+                    return "1001";
+                default:
+                    return "";
+            }
+        }
+
+        private string GetStringFromBitString(string v)
+        {
+            switch (v)
+            {
+                case "0000":
+                    return "0";
+                case "0001":
+                    return "1";
+                case "0010":
+                    return "2";
+                case "0011":
+                    return "3";
+                case "0100":
+                    return "4";
+                case "0101":
+                    return "5";
+                case "0110":
+                    return "6";
+                case "0111":
+                    return "7";
+                case "1000":
+                    return "8";
+                case "1001":
+                    return "9";
+                default:
+                    return "";
+            }
+        }
+    }
+    public static class StringExtensions
+    {
+        public static string[] SubstringAtCount(this string self, int count)
+        {
+            var result = new List<string>();
+            var length = (int)Math.Ceiling((double)self.Length / count);
+
+            for (int i = 0; i < length; i++)
+            {
+                int start = count * i;
+                if (self.Length <= start)
+                {
+                    break;
+                }
+                if (self.Length < start + count)
+                {
+                    result.Add(self.Substring(start));
+                }
+                else
+                {
+                    result.Add(self.Substring(start, count));
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
